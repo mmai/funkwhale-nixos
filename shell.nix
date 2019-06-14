0f2f9e295a9e5ac930cc695a6162385927bffc4d
Henri Bourcereau <henri.bourcereau@gmail.com>
2019-06-14 16:29:03 +0200

    funkwhale 0.18 -> 0.19

--- shell.nix
+++ shell.nix
@@ -1,36 +1,18 @@
 let
   # Use a local version of nixpkgs:
-  pkgs = (import /home/henri/Travaux/nixpkgs {});
+  # pkgs = (import /home/henri/travaux/nixpkgs {});
 
   # Use a specific version of nixpkgs:
   # cf. https://nixos.wiki/wiki/FAQ/Pinning_Nixpkgs
-  #   Commit hash as of 2019-01-28
+  #   Commit hash as of 2019-06-14
   #   `git ls-remote https://github.com/mmai/nixpkgs funkwhale`
-  # rev = "69c6ba8b5bcb47fec423ca1d296b7613d50d6801";
-  # pkgsSrc = builtins.fetchGit {
-  #   url = https://github.com/mmai/nixpkgs;
-  #   ref = "funkwhale";
-  #   rev = rev;
-  # };
-  #
-  # # Patch top-level/default.nix on nixos-unstable (cf. bug https://github.com/NixOS/nixpkgs/issues/51858)
-  # pkgs = let
-  #   hostPkgs = (import pkgsSrc {});
-  #   patches = [ ./top-level.patch ];
-  #   patchedPkgs = hostPkgs.runCommand "nixpkgs-${rev}"
-  #   {
-  #     inherit pkgsSrc;
-  #     inherit patches;
-  #   }
-  #   ''
-  #   cp -r $pkgsSrc $out
-  #   chmod -R +w $out
-  #   for p in $patches; do
-  #     echo "Applying patch $p";
-  #     patch -d $out -p1 < "$p";
-  #   done
-  #   '';
-  # in import patchedPkgs {};
+  rev = "8f2a49c652902058c0a8731106955deade8910b6";
+  pkgsSrc = builtins.fetchGit {
+    url = https://github.com/mmai/nixpkgs;
+    ref = "funkwhale";
+    rev = rev;
+  };
+  pkgs = (import pkgsSrc {});
 
 in
 pkgs.stdenv.mkDerivation rec {
